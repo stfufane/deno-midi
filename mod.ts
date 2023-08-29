ec6226151a13916f099d9e3679195f74ff22d98e
Stéphane Albanese <albanese.stephane@gmail.com>
2023-08-30 00:00:29 +0200

    More refactoring + formating and comments

--- mod.ts
+++ mod.ts
@@ -1,10 +1,14 @@
-import { rtmidi_bindings } from "./deps.ts"
+import { rtmidi_bindings } from "./deps.ts";
 
 const rtmidi = Deno.dlopen("../build/rtmidi.dll", rtmidi_bindings).symbols;
 const encoder = new TextEncoder();
 const decoder = new TextDecoder();
 
-export function get_version() : string {
+/**
+ * Get the version of the lib.
+ * @returns The current RtMidi version.
+ */
+export function get_version(): string {
   const version_buffer = rtmidi.rtmidi_get_version();
   if (version_buffer != null) {
     const version = new Deno.UnsafePointerView(version_buffer);
@@ -13,37 +17,63 @@ export function get_version() : string {
   return "not found";
 }
 
+/**
+ * Generic wrapper around a MIDI device. This class should not be used directly.
+ * Encapsulates all the common methods used by Input and Output.
+ */
 class MidiDevice {
-  protected _device! : ArrayBuffer;
+  protected _device!: ArrayBuffer;
   protected _port = -1;
   protected readonly _port_name: string;
 
-  constructor(port_name: string) {
+  constructor(port_name: string, device_ptr: Deno.PointerValue) {
     this._port_name = port_name;
+    if (device_ptr != null) {
+      this._device = Deno.UnsafePointerView.getArrayBuffer(device_ptr, 32, 0);
+    } else {
+      throw new Error("Failed to create default midi in device");
+    }
   }
 
-  get_port_count() : number {
+  protected check_error(): void {
+    // Read the 17th bit of the device to check the error.
+    const error = new Uint8Array(this._device)[16];
+    console.log(error);
+    // TODO: Try to create a pointer from the address at the end of the device buffer and read the string from it.
+  }
+
+  get_port_count(): number {
     return rtmidi.rtmidi_get_port_count(this._device);
   }
-  
-  get_ports() : string[] {
+
+  get_ports(): string[] {
     const port_name = new Uint8Array(32);
     const port_name_length = new Uint32Array(1).fill(32);
     const ports: string[] = [];
     for (let i = 0; i < this.get_port_count(); i++) {
       // Retrieve the name of the port and the length of the name.
-      const success = rtmidi.rtmidi_get_port_name(this._device, i, port_name, port_name_length);
+      const success = rtmidi.rtmidi_get_port_name(
+        this._device,
+        i,
+        port_name,
+        port_name_length,
+      );
       ports.push(decoder.decode(port_name).substring(0, success));
     }
     return ports;
   }
 
-  open_port(port: number) : void {
+  open_port(port: number): void {
     if (port >= this.get_port_count()) {
       throw new Error("Invalid port number");
     }
     this._port = port;
-    rtmidi.rtmidi_open_port(this._device, this._port, encoder.encode(this._port_name + "\0"));
+    rtmidi.rtmidi_open_port(
+      this._device,
+      this._port,
+      encoder.encode(this._port_name + "\0"),
+    );
+    this.check_error();
   }
 
   close_port(): void {
@@ -52,32 +82,29 @@ class MidiDevice {
 }
 
 export class MidiInput extends MidiDevice {
-
   constructor() {
-    super("Deno Midi In Port");
-
     const midi_in = rtmidi.rtmidi_in_create_default();
-    if (midi_in != null) {
-      this._device = Deno.UnsafePointerView.getArrayBuffer(midi_in, 32, 0);
-    } else {
-      throw new Error("Failed to create default midi in device");
-    }
+    super("Deno Midi In Port", midi_in);
   }
 }
 
 export class MidiOutput extends MidiDevice {
-
   constructor() {
-    super("Deno Midi Out Port");
     const midi_out = rtmidi.rtmidi_out_create_default();
-    if (midi_out != null) {
-      this._device = Deno.UnsafePointerView.getArrayBuffer(midi_out, 32, 0);
-    } else {
-      throw new Error("Failed to create default midi out device");
-    }
+    super("Deno Midi Out Port", midi_out);
   }
 
-  send_message(message : Uint8Array) : void {
+  /**
+   * @brief Immediately send a single message out an open MIDI output port.
+   * @param message an array of bytes describing the MIDI message
+   * @example
+   * ```ts
+   * // Send a middle C note on MIDI channel 1
+   * midi_out.send_message(new Uint8Array([0x90, 0x3C, 0x7F]));
+   * midi_out.send_message(new Uint8Array([0x80, 0x3C, 0x2F]));
+   * ```
+   */
+  send_message(message: Uint8Array): void {
     rtmidi.rtmidi_out_send_message(this._device, message, message.length);
   }
 }
--- mod_test.ts
+++ mod_test.ts
@@ -2,17 +2,23 @@ import { assertEquals } from "https://deno.land/std@0.200.0/assert/mod.ts";
 import { assert } from "https://deno.land/std@0.200.0/assert/assert.ts";
 import { get_version, MidiInput, MidiOutput } from "./mod.ts";
 
-Deno.test({ name:"get_version", permissions: { ffi: true } }, () => {
+Deno.test({ name: "get_version", permissions: { ffi: true } }, () => {
   assertEquals(get_version(), "6.0.0");
 });
 
-Deno.test({ name: "create_default_midiin_device", permissions: { ffi: true } }, () => {
-  assert(new MidiInput());
-});
+Deno.test(
+  { name: "create_default_midiin_device", permissions: { ffi: true } },
+  () => {
+    assert(new MidiInput());
+  },
+);
 
-Deno.test({ name: "create_default_midiout_device", permissions: { ffi: true } }, () => {
-  assert(new MidiOutput());
-});
+Deno.test(
+  { name: "create_default_midiout_device", permissions: { ffi: true } },
+  () => {
+    assert(new MidiOutput());
+  },
+);
 
 Deno.test({ name: "get_ports", permissions: { ffi: true } }, () => {
   const midi_out = new MidiOutput();
